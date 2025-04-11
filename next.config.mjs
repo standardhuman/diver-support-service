d99d3ff6e2cc5f52935dddd670359ac703e483f0
Brian Cline <standardhuman@gmail.com>
2025-04-11 11:52:41 -0700

    Fix deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/diver-support-service',
   images: {
     unoptimized: true,
   },
+  // Remove basePath when testing locally, uncomment when deploying to GitHub Pages
+  basePath: process.env.NODE_ENV === 'production' ? '/diver-support-service' : '',
 }
 
 export default nextConfig 
